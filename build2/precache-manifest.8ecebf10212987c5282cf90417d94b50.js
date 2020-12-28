6ee0ab85004b1866c482869faae5f492088d6948
Claudia Jäger <67694519+claudiajaeger@users.noreply.github.com>
2020-12-28 19:41:03 +0100

    Add files via upload

---
+++ build2/precache-manifest.8ecebf10212987c5282cf90417d94b50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d35bbc766f589ab3b75a35e786a92a70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41c59a13caa577628968",
+    "url": "/static/css/main.538627c3.chunk.css"
+  },
+  {
+    "revision": "91fbbfbc43edc7ae5a70",
+    "url": "/static/js/2.e17dacae.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e17dacae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41c59a13caa577628968",
+    "url": "/static/js/main.6f2ea33d.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  }
+]);
